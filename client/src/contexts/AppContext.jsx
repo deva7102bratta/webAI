175e2c729c5449e927e2a018da2c6f886392c9e6
deva7102bratta <devabrattayumnam@gmail.com>
2026-08-15 08:18:18 +0530

    created user Authentication forms

--- client/src/contexts/AppContext.jsx
+++ client/src/contexts/AppContext.jsx
@@ -1,10 +1,14 @@
 import React, { createContext, useContext, useEffect, useState } from 'react';
 import api from "../api/api"
+import toast from "react-hot-toast"
+import { useNavigate } from 'react-router-dom';
 
 const AppContext = createContext(undefined)
 
 export function AppContextProvider({children}){
 
+  const navigate = useNavigate()
+    
   // Auth States
   const [user, setUser] = useState(null)
   const [loadingUser, setLoadingUser] = useState(true)
@@ -12,7 +16,7 @@ export function AppContextProvider({children}){
   // Auth Actions
   const checkSession = async ()=>{
     try{
-      const {data} = await api.get("api/auth/me")
+      const {data} = await api.get("/api/auth/me")
       setUser(data.user)
     }catch(error){
       setUser(null)
@@ -23,11 +27,38 @@ export function AppContextProvider({children}){
 
   useEffect(() => {
     checkSession()
-  }, [checkSession])
+  }, [])
   
+  const login = async (email, password) =>{
+    try{
+      const {data} = await api.post("/api/auth/login", {email, password})
+      setUser(data.user)
+      toast.success("Welcome back!")
+      navigate("/")
+    }catch(err){
+      console.error("Login failed:", err)
+      const errMsg = err?.response?.data?.error || "Invalid email or password"
+      toast.error(errMsg)
+      throw new Error(errMsg)
+    }
+  }
+  
+  const register = async (name, email, password) =>{
+    try{
+      const {data} = await api.post("/api/auth/register", {name, email, password})
+      setUser(data.user)
+      toast.success("Account created successfully!")
+      navigate("/")
+    }catch(err){
+      console.error("Registration failed:", err)
+      const errMsg = err?.response?.data?.error || "Invalid email or password"
+      toast.error(errMsg)
+      throw new Error(errMsg)
+    }
+  }
   
   return(
-    <AppContext.Provider value={{user, loadingUser}}>
+    <AppContext.Provider value={{user, loadingUser, login, register}}>
       {children}
     </AppContext.Provider>
   )
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,7 @@ import {AppContextProvider} from "./contexts/AppContext"
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom';
+import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
